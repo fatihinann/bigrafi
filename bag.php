8ce15cb22f17e00de6025e18992b1efbc88a45ee
Fatih İnan <fatihinan3437@gmail.com>
2022-05-14 17:48:59 +0300

    php

---
+++ bag.php
@@ -0,0 +1,17 @@
+<?php
+
+try
+
+    {
+            $veritabani = new PDO("mysql:host=localhost;dbname=bigrafi_veritabani;charset=utf8;","root","");
+    } 
+
+    catch (PDOExpection $sorun)
+
+    {
+        echo $sorun->getMessage();
+
+    }
+
+
+?>
---
+++ show.php
@@ -0,0 +1,7 @@
+<?php
+
+echo 'SAG OLLLLLLLLLL';
+
+echo "<meta http-equiv='refresh' content='2;url=index.html'>";
+
+?>
